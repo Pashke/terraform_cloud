3521d7939b264393be77abe51c941269d8c49d3d
Pavel <19tipok88@gmail.com>
2022-06-26 18:23:22 +0700

    update token in provider

--- provider.tf
+++ provider.tf
@@ -18,7 +18,7 @@ terraform {
 }
 
 provider "yandex" {
-  service_account_key_file = var.token
+  token                    = var.token
   cloud_id                 = var.yandex_cloud_id
   folder_id                = var.yandex_folder_id
 }
